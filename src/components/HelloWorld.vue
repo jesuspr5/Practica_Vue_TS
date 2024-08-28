f887494a91813517147113ecfcadd9efccffeac7
jesus <pereirajesus35@gmail.com>
2024-08-28 17:21:20 -0400

    only game

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- para habilitar un componente de las practicas solo debe quitar el comentario y hab el import del componente -->
   <v-container>
-    <PracticaOne/>
+    <!-- <PracticaOne/>
     <div class="btn-container">
       <Boton @click="enviarAlert" nombre="enviar" type="submit" class="success"/> 
     </div>
@@ -13,7 +13,7 @@
   :title="items.title"
   :body="items.body"
  @accionEliminar="eliminarnoticia"
-  /> 
+  />  -->
 <GamexRaya/>
   
   </v-container>
@@ -23,40 +23,36 @@
 
 <script setup lang="ts">
 import GamexRaya from './Games/GamexRaya.vue';
+// import CardNoticias from './CardNoticias.vue';
+// import Boton from './Boton.vue';
+// import PracticaOne from './PracticaOne.vue';
+// import { ref } from 'vue';
 
+// const noticias = ref([
+// {
+//         "id": 1,
+//         "title": "Venezuela logra un acuerdo histórico con el FMI",
+//         "body": "Caracas y el Fondo Monetario Internacional han alcanzado un acuerdo preliminar para la reestructuración de la deuda, lo que podría abrir las puertas a nuevas inversiones y un crecimiento económico sostenido."
+//     },
+//     {
+//         "id": 2,
+//         "title": "Aumentan los casos de COVID-19 en Barquisimeto",
+//         "body": "Las autoridades sanitarias han reportado un incremento en el número de contagios por coronavirus en la ciudad, por lo que se exhorta a la población a mantener las medidas de bioseguridad."
+//     },
+//     {
+//         "id": 3,        
+//         "title": "Inauguran nueva línea de metro en Caracas",
+//         "body": "El presidente Nicolás Maduro encabezó la inauguración de la nueva línea del Metro de Caracas, que conectará a varios municipios del Área Metropolitana y beneficiará a millones de usuarios."
+//     }
+// ]);
 
+// const eliminarnoticia = (id:Number)=>{
+//  noticias.value = noticias.value.filter(x =>x.id !==id);
 
-
-import CardNoticias from './CardNoticias.vue';
-import Boton from './Boton.vue';
-import PracticaOne from './PracticaOne.vue';
-import { ref } from 'vue';
-
-const noticias = ref([
-{
-        "id": 1,
-        "title": "Venezuela logra un acuerdo histórico con el FMI",
-        "body": "Caracas y el Fondo Monetario Internacional han alcanzado un acuerdo preliminar para la reestructuración de la deuda, lo que podría abrir las puertas a nuevas inversiones y un crecimiento económico sostenido."
-    },
-    {
-        "id": 2,
-        "title": "Aumentan los casos de COVID-19 en Barquisimeto",
-        "body": "Las autoridades sanitarias han reportado un incremento en el número de contagios por coronavirus en la ciudad, por lo que se exhorta a la población a mantener las medidas de bioseguridad."
-    },
-    {
-        "id": 3,        
-        "title": "Inauguran nueva línea de metro en Caracas",
-        "body": "El presidente Nicolás Maduro encabezó la inauguración de la nueva línea del Metro de Caracas, que conectará a varios municipios del Área Metropolitana y beneficiará a millones de usuarios."
-    }
-]);
-
-const eliminarnoticia = (id:Number)=>{
- noticias.value = noticias.value.filter(x =>x.id !==id);
-
-}
-const enviarAlert = () =>{
-  alert('Gracias por enviarme 100$')
-}
+// }
+// const enviarAlert = () =>{
+//   alert('Gracias por enviarme 100$')
+// }
 
 
 
