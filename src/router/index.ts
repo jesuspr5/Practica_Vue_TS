9cdcfef843c0c3eab3100907ac0566fb26b4916a
jesus <pereirajesus35@gmail.com>
2025-01-20 12:31:51 -0400

    add cards

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,10 +10,28 @@ import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
+  routes: [
+    {
+      path: '/',
+      component: () => import('@/components/HelloWorld.vue'),
+      children: [
+        {
+          name: 'Cajero',
+          path: '/cajero',
+          component: () => import('@/components/PracticaOne.vue'),
+        },
+        {
+          name: 'Game',
+          path: '/game',
+          component: () => import('@/components/Games/GamexRaya.vue'),
+        },
+
+      ],
+    },
+  ]
 })
 
-// Workaround for https://github.com/vitejs/vite/issues/11804
+// Workaround for https://github.com/vitejs/vite/issues/11804 
 router.onError((err, to) => {
   if (err?.message?.includes?.('Failed to fetch dynamically imported module')) {
     if (!localStorage.getItem('vuetify:dynamic-reload')) {
